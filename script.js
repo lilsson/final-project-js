42898f88c939ece0e4bf022df5de6cb10b82df6d
lilsson <pirckhelavalile@gmail.com>
2021-12-14 18:46:17 +0300

    add burger

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-let burgerButton = document.getElementById('togglebutton');
-let 
+
+ let burgerButton = document.getElementById('togglebutton');
+ let navBar = document.getElementById('navigation-ul');
+
+ burgerButton.addEventListener('click',  function() {
+    navBar.classList.toggle('toggle');
+    burgerButton.classList.toggle('active');
+
+})

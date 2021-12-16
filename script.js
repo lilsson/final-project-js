ac2bb981671955b5baa1766b827a7a6e1d21e9e0
lilsson <pirckhelavalile@gmail.com>
2021-12-17 01:14:57 +0300

    add red

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// Burger code
  let burgerButton = document.getElementById('togglebutton');
  let navBar = document.getElementById('navigation-ul');
 
@@ -7,3 +7,108 @@
     burgerButton.classList.toggle('active');
 
 })
+
+// slider code
+
+let data = [
+    {
+        id: 1,
+        imageUrl: 'https://hips.hearstapps.com/toc.h-cdn.co/assets/16/18/3200x2473/gallery-1462570502-1967-volvo-1800s.jpg?resize=980:*',
+        title: 'Printerest',
+        url: 'https://www.pinterest.com/search/pins/?q=old%20cars&rs=typed&term_meta[]=old%7Ctyped&term_meta[]=cars%7Ctyped'
+    },
+    {
+        id: 2,
+        imageUrl: 'https://hips.hearstapps.com/toc.h-cdn.co/assets/16/18/2048x1532/gallery-1462571225-porsche-356-c-coupe.jpg?resize=980:*',
+        title: 'Printerest',
+        url: 'https://www.pinterest.com/search/pins/?q=old%20cars&rs=typed&term_meta[]=old%7Ctyped&term_meta[]=cars%7Ctyped'
+    },
+    {
+        id: 3,
+        imageUrl: 'https://hips.hearstapps.com/toc.h-cdn.co/assets/16/14/2560x1906/1968-chevrolet-corvette_1.jpg?resize=980:*',
+        title: 'Printerest',
+        url: 'https://www.pinterest.com/search/pins/?q=old%20cars&rs=typed&term_meta[]=old%7Ctyped&term_meta[]=cars%7Ctyped'
+    },
+    {
+        id: 4,
+        imageUrl: 'https://hips.hearstapps.com/toc.h-cdn.co/assets/16/14/3200x2136/1971-lamborghini-miura-sv_1.jpg?resize=980:*',
+        title: 'Printerest',
+        url: 'https://www.pinterest.com/search/pins/?q=old%20cars&rs=typed&term_meta[]=old%7Ctyped&term_meta[]=cars%7Ctyped'
+    },
+]
+
+
+let arrowLeft = document.getElementById('arrow-left');
+let sliderContent = document.getElementById('slider-content');
+let arrowRight = document.getElementById('arrow-right');
+let printerest = document.getElementById('rest');
+let sliderIndex = 0;
+
+function createAtag(item) {
+    let tag = document.createElement('a');
+    tag.setAttribute('href', item.url);
+    tag.setAttribute('class', 'slide');
+
+    return tag;
+}
+function createh2tag(item) {
+    let tagTitle = document.createElement('h2');
+    tagTitle.setAttribute('class', 'title');
+    tagTitle.append(item.title);
+
+
+    return tagTitle;
+}
+function createImgtag(item) {
+    let tagImage = document.createElement('img');
+    tagImage.setAttribute('src', item.imageUrl);
+    tagImage.setAttribute('alt', 'slide');
+
+    return tagImage;
+}
+
+
+function setSlide() {
+    sliderContent.innerHTML = ' ';
+    let slideItem = createAtag(data[sliderIndex]);
+    let h2tag = createh2tag(data[sliderIndex]);
+    let imgTag = createImgtag(data[sliderIndex]);
+
+    slideItem.appendChild(imgTag);
+    slideItem.appendChild(h2tag);
+
+    sliderContent.appendChild(slideItem);
+
+    
+
+    console.log(slideItem);
+}
+
+function arrowLeftClick() {
+    if (sliderIndex <= 0) {
+        sliderIndex = data.length -1;
+        setSlide();
+        return;
+    }
+  sliderIndex--;
+  setSlide();
+}
+
+function arrowRightClick() {
+    if ( sliderIndex >= data.length -1) {
+        sliderIndex = 0;
+        setSlide();
+        return;
+    }
+    sliderIndex++;
+    setSlide();
+}
+arrowLeft.addEventListener('click', arrowLeftClick);
+
+arrowRight.addEventListener('click', arrowRightClick);
+
+setInterval( ()=> {
+    arrowRightClick();
+}, 4000);
+
+ setSlide();
